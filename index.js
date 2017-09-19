d0a2a21d7daa10e489358444734505c3ae85debe
Ben Kielty <benkielty@bens-imac.home>
2017-09-19 18:41:03 +0100

    Created launch intent Created first intent - welcoming user to skil.  Testing and working on device.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var Alexa = require('alexa-sdk')
+var Alexa = require('alexa-sdk');
 
 
 exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context, callback);
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
+
+var handlers = {
+  'LaunchRequest': function() {
+    this.emit('Welcome');
+  },
+  'Welcome': function(){
+    this.emit(':tell', 'Welcome to Bus Times');
+  }
+}
