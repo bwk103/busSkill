18bf8da3c9c3855b5bb2c9c6ced4fcce5ce37e49
Ben Kielty <benkielty@bens-imac.home>
2017-09-19 22:21:30 +0100

    MVP Reached
    Launching Alexa returns the minutes until the next bus.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var Alexa = require('alexa-sdk');
+var https = require('https');
+var apiResponse = require('Testresult');
+// require('dotenv').config();
 
 
 exports.handler = function(event, context, callback){
@@ -9,9 +12,17 @@ exports.handler = function(event, context, callback){
 
 var handlers = {
   'LaunchRequest': function() {
-    this.emit('Welcome');
-  },
-  'Welcome': function(){
-    this.emit(':tell', 'Welcome to Bus Times');
+    var mythis = this;
+    var resultString = '';
+    https.get('https://api.tfl.gov.uk/StopPoint/40004406076A/Arrivals?app_id=5efe9e71&app_key=e4b6c1cfc9c5c20687aa8e7cb7a2470c', (res) => {
+      res.on('data', (d) => {
+        resultString += d
+      });
+      res.on('end', function(res) {
+        response = JSON.parse(resultString)
+        const output = Math.floor(parseInt(response[0].timeToStation) / 60);
+        mythis.emit(':tell', 'There are ' + output + ' minutes until the next bus.')
+      })
+    })
   }
 }
