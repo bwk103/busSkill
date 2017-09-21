6b106af13ac5321dfce1b7410c303f5489e2ea84
Ben Kielty <benkielty@bens-imac.home>
2017-09-21 15:20:51 +0100

    Amend Launch Intent
    Making small change to launch intent.

--- index.js
+++ index.js
@@ -11,18 +11,22 @@ exports.handler = function(event, context, callback){
 };
 
 var handlers = {
+
   'LaunchRequest': function() {
     var mythis = this;
     var resultString = '';
-    https.get('https://api.tfl.gov.uk/StopPoint/40004406076A/Arrivals?app_id=5efe9e71&app_key=e4b6c1cfc9c5c20687aa8e7cb7a2470c', (res) => {
+
+    https.get('https://api.tfl.gov.uk/StopPoint/40004406076A/Arrivals?app_id=APP_ID&app_key=APP_KEY', (res) => {
       res.on('data', (d) => {
         resultString += d
       });
+
       res.on('end', function(res) {
         response = JSON.parse(resultString)
         const output = Math.floor(parseInt(response[0].timeToStation) / 60);
         mythis.emit(':tell', 'There are ' + output + ' minutes until the next bus.')
-      })
+      });
+
     })
   }
 }
